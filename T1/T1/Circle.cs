f637403fa01dec0e9de1c717017fd7f114a3e5d5
Andrii Vasylyk <deadsith@hotmail.com>
2016-09-03 18:57:35 +0300

    Implemented UI, collision checks and circle respawn.

--- T1/T1/Circle.cs
+++ T1/T1/Circle.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -6,25 +7,41 @@ namespace T1
 {
     internal class Circle : Figure
     {
-        private readonly Ellipse _ellipse;
+        private Ellipse _ellipse;
+        private Ellipse _clone;
+        private Point Centre;
 
         public Circle(Canvas parent, double radius, int x, int y) : base(parent)
         {
             _ellipse = new Ellipse
             {
-                Height = radius,
-                Width = radius,
+                Height = radius * 2,
+                Width = radius * 2,
                 Stroke = Brushes.Black
             };
-            Canvas.SetTop(_ellipse, CanvasCentre.Y - y - radius / 2);
-            Canvas.SetLeft(_ellipse, CanvasCentre.X + x - radius / 2);
+            Centre = new Point(x, y);
+            Canvas.SetTop(_ellipse, CanvasCentre.Y - y - radius);
+            Canvas.SetLeft(_ellipse, CanvasCentre.X + x - radius);
             ParentCanvas.Children.Add(_ellipse);
         }
 
         public override void Move(int xChange, int yChange = 0)
         {
+            Centre += new Point(xChange, yChange);
             Canvas.SetTop(_ellipse, Canvas.GetTop(_ellipse) - yChange);
             Canvas.SetLeft(_ellipse, Canvas.GetLeft(_ellipse) + xChange);
+            var diff = ParentCanvas.ActualWidth / 2 - Math.Abs(Centre.X) + _ellipse.Width;
+            if (diff < 0)
+            {
+                Canvas.SetLeft(_ellipse, Math.Abs(diff));
+                Centre = new Point((int)(-ParentCanvas.ActualWidth / 2 + _ellipse.Width / 2), Centre.Y);
+            }
+        }
+
+        public static bool CheckCollision(Circle c1, Circle c2)
+        {
+            var d = c1.Centre - c2.Centre;
+            return Math.Sqrt(d.X * d.X + d.Y * d.Y) < (c1._ellipse.Width + c2._ellipse.Width) / 2;
         }
     }
 }
--- T1/T1/MainWindow.xaml.cs
+++ T1/T1/MainWindow.xaml.cs
@@ -12,6 +12,14 @@ namespace T1
     public partial class MainWindow : Window
     {
         private Circle _c1;
+        private Circle _c2;
+        private double _c1MoveSpeed;
+        private double _c2MoveSpeed;
+        private int _direction;
+        private int _c1Multiplier;
+        private int _c2Multiplier;
+        private DispatcherTimer _timer1;
+        private DispatcherTimer _timer2;
 
         public MainWindow()
         {
@@ -20,35 +28,75 @@ namespace T1
 
         private void DrawCanvas_Loaded(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void OnTick1(object sender, EventArgs e)
+        {
+            _c1.Move(_c1Multiplier);
+            if (Circle.CheckCollision(_c1, _c2))
+            {
+                _timer1.Stop();
+                _timer2.Stop();
+            }
+        }
+
+        private void OnTick2(object sender, EventArgs e)
+        {
+            _c2.Move(_c2Multiplier * _direction);
+            if (Circle.CheckCollision(_c1, _c2))
+            {
+                _timer1.Stop();
+                _timer2.Stop();
+            }
+        }
+
+        private void BeginButton_Click(object sender, RoutedEventArgs e)
+        {
+            DrawCanvas.Children.Clear();
             var line1 = new Line
             {
                 X1 = 0,
                 X2 = DrawCanvas.ActualWidth,
-                Y1 = DrawCanvas.ActualHeight / 2,
-                Y2 = DrawCanvas.ActualHeight / 2,
+                Y1 = DrawCanvas.ActualHeight * 2 / 3,
+                Y2 = DrawCanvas.ActualHeight * 2 / 3,
                 StrokeThickness = 2,
                 Stroke = Brushes.Black
             };
             DrawCanvas.Children.Add(line1);
-            var line2 = new Line
+            _c1Multiplier = _c2Multiplier = 1;
+            _c1MoveSpeed = int.Parse(C1Box.Text);
+            while (_c1MoveSpeed > 101)
             {
-                X1 = DrawCanvas.ActualWidth / 2,
-                X2 = DrawCanvas.ActualWidth / 2,
-                Y1 = 0,
-                Y2 = DrawCanvas.ActualHeight,
-                StrokeThickness = 2,
-                Stroke = Brushes.Black
-            };
-            DrawCanvas.Children.Add(line2);
-            _c1 = new Circle(DrawCanvas, 50, 20, 20);
-            var timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(16.6) };
-            timer.Tick += OnTick;
-            timer.Start();
-        }
-
-        private void OnTick(object sender, EventArgs e)
-        {
-            _c1.Move(5, 3);
+                _c1Multiplier *= 2;
+                _c1MoveSpeed /= 2;
+            }
+            _c2MoveSpeed = int.Parse(C2Box.Text);
+            while (_c2MoveSpeed > 101)
+            {
+                _c2Multiplier *= 2;
+                _c2MoveSpeed /= 2;
+            }
+            if (DirectionBox.SelectedIndex == 1)
+            {
+                _direction = -1;
+                var c2Radius = double.Parse(C2RadiusBox.Text);
+                _c2 = new Circle(DrawCanvas, c2Radius, (int)(DrawCanvas.ActualWidth / 2 - c2Radius),
+                    (int)(-DrawCanvas.ActualHeight / 6 + c2Radius));
+            }
+            else
+            {
+                _direction = 1;
+                var c2Radius = double.Parse(C2RadiusBox.Text);
+                _c2 = new Circle(DrawCanvas, c2Radius, 0, (int)(-DrawCanvas.ActualHeight / 6 + c2Radius));
+            }
+            var c1Radius = double.Parse(C1RadiusBox.Text);
+            _c1 = new Circle(DrawCanvas, c1Radius, (int)(-DrawCanvas.ActualWidth / 2 + c1Radius), (int)(-DrawCanvas.ActualHeight / 6 + c1Radius));
+            _timer1 = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(1000.0 / _c1MoveSpeed) };
+            _timer1.Tick += OnTick1;
+            _timer2 = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(1000.0 / _c2MoveSpeed) };
+            _timer2.Tick += OnTick2;
+            _timer2.Start();
+            _timer1.Start();
         }
     }
 }
