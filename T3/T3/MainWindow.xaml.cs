f94115a12b1c2cce86f724c6ebfd7d17f83d5858
Andrii Vasylyk <deadsith@hotmail.com>
2016-09-19 19:52:35 +0300

    Fixed sorting in T3
    T3 is fully working. Only small features remain.

--- T3/T3/MainWindow.xaml.cs
+++ T3/T3/MainWindow.xaml.cs
@@ -24,9 +24,12 @@ namespace T3
         private List<ShapePoint> Shape1;
         private List<ShapePoint> Shape2;
         private List<ShapePoint> CurrentShape;
+        private List<ShapePoint> Intersections;
 
         private Polygon _line;
         private bool _first = true;
+        private int interCount = 0;
+
 
         public MainWindow()
         {
@@ -41,10 +44,11 @@ namespace T3
                 StrokeThickness = 2
             };
             DrawCanvas.Children.Add(_line);
-            var l = new List<int>();
-            l.Add(1);
-            l.Add(2);
-            l.Insert(1,3);
+            //var l = new List<int>();
+            //l.Add(1);
+            //l.Add(2);
+            //l.Add(4);
+            //l.Insert(1,3);
         }
 
         private void DrawCanvas_MouseUp(object sender, MouseButtonEventArgs e)
@@ -125,50 +129,115 @@ namespace T3
         private void FindButton_Click(object sender, RoutedEventArgs e)
         {
             //Якщо за годинниковою стрілкою, то сама верхня завжде іде першою
+            AddIntersections();
+            var sCopy = new List<ShapePoint>(Shape1);
+            //wrong insert
+            foreach (var point in Shape1)
+            {
+                point.SortIntersections();
+                var index = point.FindIndex(sCopy);
+                sCopy.InsertRange(index+1,point.Intersections);
+            }
+            Shape1 = sCopy;
+            sCopy = new List<ShapePoint>(Shape2);
+            foreach (var point in Shape2)
+            {
+                point.SortIntersections();
+                var index = point.FindIndex(sCopy);
+                sCopy.InsertRange(index + 1, point.Intersections);
+            }
+            for (var i = 0; i < Shape1.Count; i++)
+                Shape1[i].Index1 = i;
+            Shape2 = sCopy;
+            for (var i = 0; i < Shape2.Count; i++)
+                Shape2[i].Index2 = i;
+            var allVisited = false;
+            while (!allVisited)
+            {
+                sCopy = new List<ShapePoint>();
+                var currentShape = Shape1;
+                var start = FindStart();
+                start.Visited = true;
+                var ind = start.FindIndex(currentShape) + 1 != currentShape.Count ? start.FindIndex(currentShape) + 1 : 0;
+                var current = currentShape[ind];
+                sCopy.Add(start);
+                do
+                {
+                    sCopy.Add(current);
+                    if (current.Type == ShapePoint.ShapeType.Intersection)
+                    {
+                        current.Visited = true;
+                        currentShape = currentShape == Shape1 ? Shape2 : Shape1;
+                    }
+                    var index = currentShape == Shape1 ? current.Index1 + 1 : current.Index2 + 1;
+                    if (index == currentShape.Count)
+                        index = 0;
+                    current = currentShape[index];
+                } while (current != start);
+                _line = new Polygon {Fill = Brushes.Gold};
+                foreach (var point in sCopy)
+                {
+                    _line.Points.Add(point.Coordinates);
+                }
+                DrawCanvas.Children.Add(_line);
+                allVisited = Intersections.All(point => point.Visited);
+            }
+        }
+
+        private ShapePoint FindStart()
+        {
+            foreach (ShapePoint t in Shape1)
+            {
+                if (t.Type == ShapePoint.ShapeType.Intersection && !t.Visited)
+                    return t;
+            }
+            throw  new ArgumentException();
+        }
+
+        private void AddIntersections()
+        {
+            Intersections = new List<ShapePoint>();
             ShapePoint inter;
-            var c = 0;
-            var s1Copy = new List<ShapePoint>(Shape1);
-            var s1Shift = 1;
-            var s2Copy = new List<ShapePoint>(Shape2);
-            var s2Shift = 1;
             for (var i = 0; i < Shape1.Count - 1; i++)
             {
                 for (var j = 0; j < Shape2.Count - 1; j++)
                 {
                     if (FindIntersection(Shape1[i], Shape1[i + 1], Shape2[j], Shape2[j + 1], out inter))
                     {
-                        s1Copy.Insert(i+s1Shift,inter);
-                        s2Copy.Insert(j+s2Shift,inter);
-                        s1Shift++;
-                        s2Shift++;//Correct
+                        Draw(inter);
+                        Shape1[i].Intersections.Add(inter);
+                        Shape2[j].Intersections.Add(inter);
                     }
                 }
-                //Todo: fix pasting
-                if (FindIntersection(Shape1[i], Shape1[i + 1], Shape2[Shape2.Count-1], Shape2[0], out inter))
+                if (FindIntersection(Shape1[i], Shape1[i + 1], Shape2[Shape2.Count - 1], Shape2[0], out inter))
                 {
-                    s1Copy.Insert(i + s1Shift, inter);
-                    s1Shift++;
-                    s2Copy.Insert(s2Copy.Count,inter);
+                    Draw(inter);
+                    Shape1[i].Intersections.Add(inter);
+                    Shape2[Shape2.Count - 1].Intersections.Add(inter);
                 }
+                //Todo: fix pasting
             }
             for (var j = 0; j < Shape2.Count - 1; j++)
             {
                 if (FindIntersection(Shape1[Shape1.Count-1], Shape1[0], Shape2[j], Shape2[j + 1], out inter))
                 {
-                    s1Copy.Insert(s1Copy.Count,inter);
-                    s2Copy.Insert(j + s2Shift, inter);
-                    s2Shift++;
+                    Draw(inter);
+                    Shape1[Shape1.Count - 1].Intersections.Add(inter);
+                    Shape2[j].Intersections.Add(inter);
                 }
             }
             if (FindIntersection(Shape1[Shape1.Count - 1], Shape1[0], Shape2[Shape2.Count - 1], Shape2[0], out inter))
             {
-                s1Copy.Insert(s1Copy.Count, inter);
-                s2Copy.Insert(s2Copy.Count, inter);
+                Draw(inter);
+                Shape1[Shape1.Count - 1].Intersections.Add(inter);
+                Shape2[Shape2.Count - 1].Intersections.Add(inter);
             }
         }
 
         private void Draw(ShapePoint inter)
         {
+            interCount++;
+            Intersections.Add(inter);
             var d = new Ellipse
             {
                 Width = 5,
--- T3/T3/ShapePoint.cs
+++ T3/T3/ShapePoint.cs
@@ -19,6 +19,11 @@ namespace T3
         public double X => Coordinates.X;
         public double Y => Coordinates.Y;
         public ShapeType Type;
+        public List<ShapePoint> Intersections = new List<ShapePoint>();
+        public bool Visited = false;
+
+        public int Index1;
+        public int Index2;
 
         public ShapePoint(Point p)
         {
@@ -40,6 +45,53 @@ namespace T3
                     return i;
             }
             throw new ArgumentException();
+        }
+
+        public void SortIntersections()
+        {
+            if (Intersections.Count <= 1) return;
+            var sortByY = (this.X - Intersections[0].X) < (this.Y - Intersections[0].Y);
+            int koef = 1;
+            if (sortByY)
+            {
+                if (Y > Intersections[0].Y)
+                    koef = -1;
+            }
+            else
+            {
+                if (X > Intersections[0].X)
+                    koef = -1;
+            }
+            //sort by closest to point = first
+            Intersections.Sort((p1, p2) =>
+            {
+                if (sortByY)
+                {
+                    if (p1.Y < p2.Y)
+                        return -1*koef;
+                    else if (p1.Y > p2.Y)
+                        return 1*koef;
+                }
+                else
+                {
+                    if (p1.X<p2.X)
+                        return -1*koef;
+                    if (p1.X > p2.X)
+                        return 1*koef;
+                }
+                return 0;
+                //var d1 = DistanceSquared(p1, this);
+                //var d2 = DistanceSquared(p2, this);
+                //if (d1 - d2 < 0.01) return 0;
+                //if (d1 < d2) return -1;
+                //if (d1 > d2) return 1;
+                //return 0;
+            });
+        }
+
+        private static double DistanceSquared(ShapePoint p1, ShapePoint p2)
+        {
+            return (p2.X - p1.X) * (p2.X - p1.X) + (p2.Y - p1.Y) * (p2.Y - p1.Y);
         }
     }
 }
